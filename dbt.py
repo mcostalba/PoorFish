841c48c3abee60abc7055f28d08b7ff1f8e0422d
mcostalba <mcostalba@gmail.com>
2016-10-16 22:16:19 +0200

    Keep engine open
    And rename the script to dbt.py

--- dbt.py
+++ dbt.py
@@ -8,9 +8,6 @@ from subprocess import Popen, PIPE
 
 ON_POSIX = 'posix' in sys.builtin_module_names
 
-sf = 'C:\Users\marco\Documents\programmi\stockfish\src\stockfish.exe'
-test_epd = 'C:\Users\marco\Documents\programmi\stockfish\src\hard2016beta6.epd'
-
 def read_epd(file):
     epd = []
     cnt = 0
@@ -21,13 +18,12 @@ def read_epd(file):
                 cnt += 1
     return epd, cnt
 
-def run(engine, fen, time, hash=1024, threads=3):
-    cmd = ['setoption name Hash value ' + str(hash),
-           'setoption name Threads value ' + str(threads),
+def run(args, fen):
+    cmd = ['setoption name Hash value ' + str(args.hash),
+           'setoption name Threads value ' + str(args.threads),
            'position fen ' + fen,
-           'go movetime ' + str(time)]
-
-    p = Popen(engine, stdout=PIPE, stdin=PIPE, universal_newlines=True, close_fds=ON_POSIX)
+           'go movetime ' + str(args.movetime)]
+    p = args.process
     p.stdin.write('\n'.join(cmd) + '\n') # Note the trailing '\n'
     line = ''
     while 'bestmove' not in line:
@@ -35,8 +31,6 @@ def run(engine, fen, time, hash=1024, threads=3):
         if 'score' in line:
             score = line.split('score ')[-1].split(' nodes')[0].strip()
     bestMove = line.split('bestmove')[1].strip().split(' ')[0]
-    p.stdin.close()
-    exit_code = p.wait()
     return bestMove, score
 
 def parse_position(line):
@@ -55,7 +49,7 @@ def parse_position(line):
                 return '', '', '', "Invalid fen\n\n".format(attempts[0])
     san = san.split(';')[0]
     san = san.replace(',', ' ').replace('!', ' ').split()[0]
-    attempts = [san, san + '+', san + '#'] # Be forgiving on the san format
+    attempts = [san, san + '+'] # Be forgiving on the san format
     for san in attempts:
         try:
             board.push_san(san)
@@ -77,61 +71,63 @@ def compare(score1, score2):
     score2 = score2.split('cp')[1].strip()
     return int(score1) > -int(score2)
 
-def run_session(engine, test_epd, time_per_move, result_epd):
-    epd, total = read_epd(test_epd)
+def run_session(args):
+    open(args.result_epd, 'w').close() # Clear output file
+    epd, total = read_epd(args.testsuite)
+    args.process = Popen(args.engine, stdout=PIPE, stdin=PIPE, universal_newlines=True, close_fds=ON_POSIX)
     cnt = 0
-    open(result_epd, 'w').close()
     for i in range(1, len(epd)):
         pos = epd[i-1].strip()
         (fen, san, new_fen, result) = parse_position(pos)
         if result == 'skip line':
-            append_result(result_epd, '\n')
+            append_result(args.result_epd, '\n')
             continue
         cnt += 1
         print("Position: {}/{}\nPos: {}".format(cnt, total, pos))
         if result != 'OK':
             print(result)
-            append_result(result_epd, pos + '\n')
+            append_result(args.result_epd, pos + '\n')
             continue
-        (bestmove, score1) = run(engine, fen, time_per_move)
+        (bestmove, score1) = run(args, fen)
         board = chess.Board(fen)
         bestmove = board.san(chess.Move.from_uci(bestmove))
         print("Warm-up best move: {}, score: {}".format(bestmove, score1))
         if (bestmove == san):
             print("Best move already found!\n\n")
-            append_result(result_epd, '\n')
+            append_result(args.result_epd, '\n')
             continue
         print("Forcing best move: {}".format(san))
-        (bestmove, score2) = run(engine, new_fen, time_per_move)
+        (bestmove, score2) = run(args, new_fen)
         print("After forcing best move, score: {}\n\n".format(score2))
         if compare(score1, score2):
-            append_result(result_epd, pos + '\n')
+            append_result(args.result_epd, pos + '\n')
         else:
-            append_result(result_epd, '\n')
+            append_result(args.result_epd, '\n')
+    args.process.stdin.close() # Make the engine to quit
+    args.process.wait()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run DBT test on a epd testsuite')
     parser.add_argument("engine", help="Path to the chess engine")
     parser.add_argument("testsuite", help="Path to the epd testsuite file")
     parser.add_argument("movetime", help="Time for each position in milliseconds", type=int)
+    parser.add_argument("--threads", help="Number of threads", type=int, default=3)
+    parser.add_argument("--hash", help="Hash table size in MB", type=int, default=1024)
     args = parser.parse_args()
-    engine = args.engine
-    tesuite = args.testsuite
-    movetime = args.movetime
 
-    if not os.path.isfile(engine):
-        print("Engine {} not found.".format(engine))
+    if not os.path.isfile(args.engine):
+        print("Engine {} not found.".format(args.engine))
         sys.exit(0)
 
-    if not os.path.isfile(tesuite):
-        print("Testsuite {} not found.".format(testsuite))
+    if not os.path.isfile(args.testsuite):
+        print("Testsuite {} not found.".format(args.testsuite))
         sys.exit(0)
 
-    result_epd = tesuite.split('.epd')[0]
-    result_epd += '_' + str(movetime / 1000) + 'sec.epd'
+    fname = args.testsuite.split('.epd')[0]
+    args.result_epd = fname + '_' + str(args.movetime / 1000) + 'sec.epd'
 
-    print("Running DBT on {}, successful positions will be written \ninto {},"
-          "preserving line number.\nEach position will be searched for "
-          "{} milliseconds.\n".format(tesuite, result_epd, movetime))
+    print("Running DBT on {}, successful positions will be written \ninto {}, "
+          "preserving line number. Each position will \nbe searched for "
+          "{} milliseconds.\n".format(args.testsuite, args.result_epd, args.movetime))
 
-    run_session(engine, tesuite, movetime, result_epd)
+    run_session(args)
